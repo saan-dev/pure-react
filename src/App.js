ee36245b0bf1dadd47d2a31c4c2b3d63f12ddd04
SaanBitbucket <saanrashid@gmail.com>
2020-04-11 12:59:02 +0200

    Async & Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
